95511508f5aa2258d5ed553516796a3d0c6d3458
Leo <leonis13579@gmail.com>
2020-04-04 15:57:03 +0300

    Added to 04.04.2020

---
+++ Assets/_Scripts/GetInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetInfo : MonoBehaviour
+{
+    enum Hand { right, left };
+    [SerializeField]
+    Hand hand;
+    private void OnEnable()
+    {
+        StartCoroutine(Check());
+    }
+
+    IEnumerator Check()
+    {
+        try
+        {
+            if (hand == Hand.left)
+            {
+                GameObject.FindGameObjectWithTag("Game Pipeline").GetComponent<EggSpown>().RefreshHeath();
+            }
+            else if (hand == Hand.right)
+            {
+                GameObject.FindGameObjectWithTag("Game Pipeline").GetComponent<EggSpown>().RefreshHeath();
+            }
+        }
+        catch { }
+        yield return null;
+    }
+}
